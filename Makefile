faca8d90e2c21778643c5ccf82a233cf207cbb9d
bodhi <blevaka@gmail.com>
2017-01-14 08:14:20 +0700

    1

---
+++ Makefile
@@ -0,0 +1,3 @@
+all:
+	gcc main.c -o csort
+	./csort < data.txt
---
+++ main.c
@@ -0,0 +1,25 @@
+//load file to array and sort then
+#include <stdio.h>
+#include <stdio.h>
+
+
+int main(){
+/*
+int array_length=1;
+int *array;
+int *newarray;
+
+newarray = (int *)
+    realloc(array, (array_length + 1) * sizeof(int));
+if (!newarray)
+    error("out of memory");
+array = newarray;
+array[array_length++] = x;*/
+	int i;
+	do{
+	 scanf("%d\n",&i);
+	printf("%d\n",i);
+	}while(i != NULL);
+
+	return 0;
+}
